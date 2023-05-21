b4829b5de78ecd6b9ecadf2100917425b80b0680
kali <andreimaier311@gmail.com>
2023-05-21 07:29:29 -0400

    [+] Added XOR Encryption

--- StegClone.py
+++ StegClone.py
@@ -2,20 +2,35 @@ import cv2
 import numpy as np
 import argparse
 import pyfiglet
+import os
+
+from itertools import product
 
 
-def embed(cover_image_path, secret_image_path, output_path):
+def xor(data, key):
+    index = 0
+    for i, j in product(range(data.shape[0]), range(data.shape[1])):
+        for channel in range(3):
+            data[i, j, channel] = data[i, j, channel] ^ ord(key[index % len(key)])
+            index += 1
+
+    return data
+
+
+def embed(cover_image_path, secret_image_path, output_path, xor_key):
     cover_image = cv2.imread(cover_image_path)
     secret_image = cv2.imread(secret_image_path)
 
     # Resize the secret image to match the size of the cover image
     secret_image_resized = cv2.resize(secret_image, (cover_image.shape[1], cover_image.shape[0]))
 
+    secret_image_xor = xor( secret_image_resized, xor_key )
+
     cover_channels = cv2.split(cover_image)
 
     # LSB Technique
     stego_channels = []
-    for channel, secret_channel in zip(cover_channels, cv2.split(secret_image_resized)):
+    for channel, secret_channel in zip(cover_channels, cv2.split(secret_image_xor)):
         lsb_cover = channel & 0xFE
         msb_secret = secret_channel >> 7
         stego_channel = lsb_cover | msb_secret
@@ -25,25 +40,30 @@ def embed(cover_image_path, secret_image_path, output_path):
     stego_image = cv2.merge(stego_channels)
     cv2.imwrite(output_path, stego_image)
 
-    print("[+] Embedding performed.")
+    print("[+] Embedding performed. Path to file:\n[+] " + os.path.abspath(output_path))
 
 
-def decode(stego_image_path, output_path):
+def decode(stego_image_path, output_path, xor_key):
     stego_image = cv2.imread(stego_image_path)
 
     stego_channels = cv2.split(stego_image)
 
     secret_channels = []
 
-    # Reverse LSB steganography
+    # Reverse LSB Technique
     for stego_channel in stego_channels:
         lsb_stego = stego_channel & 0x01
         secret_channel = lsb_stego << 7
         secret_channels.append(secret_channel)
+ 
+    secret_xor_image = cv2.merge(secret_channels)
 
-    secret_image = cv2.merge(secret_channels)
+    # Xor Decryption
+    secret_image = xor(secret_xor_image, xor_key)
 
     cv2.imwrite(output_path, secret_image)
+
+    print("[+] Decoding performed. Path to file:\n[+] " + os.path.abspath(output_path))
 
 
 
@@ -68,13 +88,13 @@ def main():
             print("[-] Secret Path Missing... Exiting")
             return
         if args.output == None:
-            embed( args.image_path, args.secret_path, 'example_images/stego_image.png' )
+            embed( args.image_path, args.secret_path, 'example_images/stego_image.png', args.key )
         else:
-            embed( args.image_path, args.secret_path, args.output )
+            embed( args.image_path, args.secret_path, args.output, args.key )
 
     if args.action == 'decode':
         if args.output == None:
-            decode(args.image_path, 'example_images/decoded.png' )
+            decode(args.image_path, 'example_images/decoded.png', args.key )
         else:
             decode(args.image_path, args.output )
 
